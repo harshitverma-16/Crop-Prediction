88c9fac9f28458ad78d7e2e83263385e517f0e7b
Harshit <itsharshit45@gmail.com>
2025-08-01 22:31:57 +0530

    model integration

---
+++ app.py
@@ -0,0 +1,23 @@
+import numpy as np
+import joblib
+from flask import Flask, request, render_template, jsonify
+
+model = joblib.load('crop_model.pkl')
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    float_feature = [float(x) for x in request.form.values()]
+    features = np.array([float_feature])
+    prediction = model.predict(features)
+    predicted_crop = prediction[0]
+    return render_template('index.html', prediction_text=f'Predicted Crop: {predicted_crop}')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
